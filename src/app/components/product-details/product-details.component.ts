6613fdbfb02f893b588e9548fae2f4a72b449388
sudheesha <sudheesha@live.com>
2023-08-14 16:18:35 +1200

    add routing

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent {
-
+  pageTitle: string = 'Product Details';
 }
